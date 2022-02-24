df246edd5d44b5d004885ba38c1bbd45292e299f
Xevenst <stevenjoentoro@gmail.com>
2022-02-24 17:45:59 +0800

    Fixed bugs, able to swipe different directionm in the same time

--- Chromesthesia/Assets/AssetsNote/NotesAsset-main/Scripts/Gameplay/GameMaster.cs
+++ Chromesthesia/Assets/AssetsNote/NotesAsset-main/Scripts/Gameplay/GameMaster.cs
@@ -17,6 +17,14 @@ public class GameMaster : MonoBehaviour
         public double time;
         public float posx,posy;
         public string direction;
+        public string status;
+        public Note()
+        {
+            this.time = 0;
+            this.posx = 0;
+            this.posy = 0;
+            this.direction = "";
+        }
         public Note(double time,float posx,float posy,string direction)
         {
             this.time = time;
@@ -36,9 +44,10 @@ public class GameMaster : MonoBehaviour
     public double time;
     public Text timetext;
     // Start is called before the first frame update
-    public Queue<Note> que = new Queue<Note>();
-    public Queue<Note> isoutthere = new Queue<Note>();
-    public Queue<Note> touchable = new Queue<Note>();
+    public static Queue<Note> que = new Queue<Note>();
+    public static Queue<Note> isoutthere = new Queue<Note>();
+    public static Queue<Note> touchable = new Queue<Note>();
+    public static List<(double timing, List<Note> notes)> sametime = new List<(double timing, List<Note> notes)>();
 
     void Start()
     {
@@ -47,7 +56,6 @@ public class GameMaster : MonoBehaviour
         timetext.text = time.ToString();
         TextAsset theList = Resources.Load<TextAsset>("Beatmap/queuenumber");
         string text = theList.text;
-        // char trigger='\n';
         string[] words = text.Split('\n');
         foreach(string notes in words)
         {
@@ -66,87 +74,132 @@ public class GameMaster : MonoBehaviour
     {
         time = Time.timeAsDouble;
         timetext.text = time.ToString();
-        currentscore.text = score.ToString();
-        if(que.Count==0)
-        {
-            // Debug.Log("Queue empty");
+        if(sametime.Count > 0){
+            // while(time>sametime[0].timing){
+            //     sametime.Remove(sametime[0]);
+            // }
         }
-        else if(time >= que.Peek().time)
-        {
-            while(que.Count!=0 && que.Peek().time <= time)
+        if(que.Count > 0){
+            if(time >= que.Peek().time)
             {
-                GameObject create = Instantiate(circle);
-                circle.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
-                create.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(que.Peek().posx, que.Peek().posy,1));
-                // create.transform.rotation = Quaternion.Euler(90,90, 90);
-                Note temp = new Note(que.Peek().time+1f,que.Peek().posx,que.Peek().posy,que.Peek().direction);
-                temp.setGameObject(create);
-                isoutthere.Enqueue(temp);
-                que.Dequeue();
+                List<Note> Temp = new List<Note>();
+                Note last = new Note();
+                while(que.Count!=0 && que.Peek().time <= time)
+                {
+                    GameObject create = Instantiate(circle);
+                    circle.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+                    create.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(que.Peek().posx, que.Peek().posy,1));
+                    // create.transform.rotation = Quaternion.Euler(90,90, 90);
+                    Note temp = new Note(que.Peek().time+1f,que.Peek().posx,que.Peek().posy,que.Peek().direction);
+                    temp.setGameObject(create);
+                    isoutthere.Enqueue(temp);
+                    Temp.Add(temp);
+                    last = temp;
+                    que.Dequeue();
+                }
+                sametime.Add((last.time,Temp));
+                Debug.Log("SMAETIME XCIOBRT: "+ sametime[0].timing);
             }
         }
-        if(isoutthere.Count==0)
+        if(isoutthere.Count > 0)
         {
-            //Debug.Log("Isoutthere empty");
-        }
-        else if(time >= isoutthere.Peek().time)
-        {
-            while(isoutthere.Count!=0 && isoutthere.Peek().time <= time)
+            if(time >= isoutthere.Peek().time)
             {
-                touchable.Enqueue(isoutthere.Peek());
-                //Debug.Log(touchable.Peek().time);
-                isoutthere.Dequeue();
+                while(isoutthere.Count!=0 && isoutthere.Peek().time <= time)
+                {
+                    touchable.Enqueue(isoutthere.Peek());
+                    isoutthere.Dequeue();
+                }
             }
         }
         if(Input.touchCount>0)
         {
             for(int i=0;i<Input.touchCount;i++)
             {
-                Touch touch = Input.GetTouch(0);
-                Vector2 startPos = touch.rawPosition;
+                // Touch touch = Input.GetTouch(0);
+                Touch[] touches = Input.touches;
+                Vector2 startPos = touches[i].rawPosition;
                 // Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
-                if(touch.phase == TouchPhase.Ended)
+                if(touches[i].phase == TouchPhase.Ended)
                 {
+                    double timetouched = time;
                     Vector2 endPos = Input.touches[i].position;
-                    // double rad = Mathf.Atan(((endPos.y-startPos.y)/(endPos.x-startPos.x)));
-                    double rad = Mathf.Atan2(touch.deltaPosition.y,touch.deltaPosition.x); 
+                    // Debug.Log(touches[i].deltaPosition);
+                    double rad = Mathf.Atan2(touches[i].deltaPosition.y,touches[i].deltaPosition.x); 
                     double angle = rad * (180 / Math.PI);
                     NoteDiamond s = touchable.Peek().getGameObject().GetComponent<NoteDiamond>();
-                    Debug.Log(angle);
-                    if(check(angle,touchable.Peek().direction)==true && checktime(touchable.time+))
+                    // Debug.Log(angle);
+                    if(getDir(angle,touchable.Peek().time)==true)
                     {
                         s.setState("perfect");
                     }
                     else
+                    {
                         s.setState("miss");
+                    }
                     touchable.Dequeue();
                 }
             }
         }
     }
 
-    public bool check(double angle,string truecircle)
+    /*
+        Direction :
+        w = up;
+        q = upleft;
+        a = left;
+        z = downleft;
+        x = down;
+        c = downright;
+        d = right;
+        e = upright;
+    */
+
+    private string checkDir(double angle)
     {
         string dir="";
         if(angle >=-22.5 && angle < 22.5)
-            dir = "right";
+            dir = "d";
         else if(angle >=22.5 && angle<67.5)
-            dir = "upright";
+            dir = "e";
         else if(angle >=67.5 && angle<112.5)
-            dir = "up";
+            dir = "w";
         else if(angle >=112.5 && angle<157.5)
-            dir = "upleft";
-        else if(angle >=157.5 && angle<-157.5)
-            dir = "left";
-        else if(angle <=-157.5 && angle<-112.5)
-            dir = "downleft";
-        else if(angle <=-112.5 && angle<-67.5)
-            dir = "down";
-        else if(angle <=-67.5 && angle<-22.5)
-            dir = "downright";
-        Debug.Log(dir);
-        if(dir==truecircle)
-            return true;
+            dir = "q";
+        else if((angle >=157.5 && angle <=180) || (angle<-157.5 && angle >=-180))
+            dir = "a";
+        else if(angle >=-157.5 && angle<-112.5)
+            dir = "z";
+        else if(angle >=-112.5 && angle<-67.5)
+            dir = "x";
+        else if(angle >=-67.5 && angle<-22.5)
+            dir = "c";
+        return dir;
+    }
+
+    private bool getDir(double angle,double time)
+    {
+        Debug.Log("timing = "+time);
+        Debug.Log(sametime.Count);
+        int now = sametime.FindIndex(x => x.timing >= time);
+        foreach((double t,List<Note> n) s in sametime){
+            Debug.Log("AVAIL TIME = "+s.t);
+        }
+        string dir = checkDir(angle);
+        Debug.Log("now = "+now+", dir = "+dir);
+        if(now != -1){
+            string dirs = "";
+            foreach(Note note in sametime[now].notes){
+                dirs += note.direction + ", ";
+            }
+            Debug.Log(time+" : (input)"+dir+", (selection)"+dirs);
+            if(sametime[now].notes.FindIndex(x => x.direction[0] == dir[0]) != -1) {
+                Note sameDirNote = sametime[now].notes.Find(x => x.direction[0] == dir[0]);
+                Debug.Log("Removing "+sameDirNote.direction);
+                sametime[now].notes.Remove(sameDirNote);
+                return true;
+            }
+        }
         return false;
     }
 }
