f060562976c1f36f0f20474248000b90ab23f857
Xevenst <stevenjoentoro@gmail.com>
2022-03-02 17:02:06 +0800

    Adding duration,type, and changed the format for Note constructor

--- Chromesthesia/Assets/AssetsNote/NotesAsset-main/Scripts/Gameplay/GameMaster.cs
+++ Chromesthesia/Assets/AssetsNote/NotesAsset-main/Scripts/Gameplay/GameMaster.cs
@@ -14,9 +14,11 @@ public class GameMaster : MonoBehaviour
     public class Note
     {
         public GameObject obj;
-        public double time;
+        public int type;
         public float posx,posy;
         public string direction;
+        public double duration;
+        public double time;
         public Note()
         {
             this.time = 0;
@@ -24,12 +26,14 @@ public class GameMaster : MonoBehaviour
             this.posy = 0;
             this.direction = "";
         }
-        public Note(double time,float posx,float posy,string direction)
+        public Note(int type,float posx,float posy,string direction,double duration,double time)
         {
-            this.time = time;
+            this.type = type;
             this.posx = posx;
             this.posy = posy;
             this.direction = direction;
+            this.duration = duration;
+            this.time = time;
         }
         public void setGameObject(GameObject obj)
         {
@@ -53,17 +57,22 @@ public class GameMaster : MonoBehaviour
         score=0;
         time = Time.timeAsDouble;
         timetext.text = time.ToString();
-        TextAsset theList = Resources.Load<TextAsset>("Beatmap/queuenumber");
+        TextAsset theList = Resources.Load<TextAsset>("Beatmap/queuenumber"); //read a textfile from "path" note that it only reads from folder Resources, so you have to put everything (that you said to Load) in resources folder, however you may make any folder inside th resouce folder.
         string text = theList.text;
         string[] words = text.Split('\n');
-        foreach(string notes in words)
+        List<string> wordlist = new List<string>(words);
+        int bodyindex = wordlist.FindIndex(x => x.Contains("<body>"))+1;
+        Debug.Log(bodyindex);
+        for(int i=bodyindex;wordlist[i]!="";i++) //read from after the <body> in the textfile and split it for later use
         {
-            string[] list = notes.Split(',');
-            double time = Convert.ToDouble(list[0]);
+            string[] list = wordlist[i].Split(',');
+            int type = Convert.ToInt16(list[0]);
             float posx = Convert.ToSingle(list[1]);
             float posy = Convert.ToSingle(list[2]);
             string direction = list[3];
-            que.Enqueue(new Note(time,posx,posy,direction));
+            double duration = Convert.ToDouble(list[4]);
+            double time = Convert.ToDouble(list[5]);
+            que.Enqueue(new Note(type,posx,posy,direction,duration,time));
         }
         currentscore.text = score.ToString();
     }
@@ -73,11 +82,6 @@ public class GameMaster : MonoBehaviour
     {
         time = Time.timeAsDouble;
         timetext.text = time.ToString();
-        if(sametime.Count > 0){
-            // while(time>sametime[0].timing){
-            //     sametime.Remove(sametime[0]);
-            // }
-        }
         if(que.Count > 0){
             if(time >= que.Peek().time)
             {
@@ -89,15 +93,14 @@ public class GameMaster : MonoBehaviour
                     circle.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
                     create.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(que.Peek().posx, que.Peek().posy,1));
                     create.transform.rotation = Quaternion.Euler(0,0,assignrotate(que.Peek().direction[0]));
-                    Note temp = new Note(que.Peek().time+1f,que.Peek().posx,que.Peek().posy,que.Peek().direction);
-                    temp.setGameObject(create);
-                    isoutthere.Enqueue(temp);
-                    Temp.Add(temp);
+                    Note temp = new Note(que.Peek().type,que.Peek().posx,que.Peek().posy,que.Peek().direction,que.Peek().duration,que.Peek().time+1f);
+                    temp.setGameObject(create); //assigning which gameobject to the temp
+                    isoutthere.Enqueue(temp); //showing the note and put in the queue
+                    Temp.Add(temp); //the list note of which when there are multiple touchable at the same time
                     last = temp;
                     que.Dequeue();
                 }
                 sametime.Add((last.time,Temp));
-                Debug.Log("SMAETIME XCIOBRT: "+ sametime[0].timing);
             }
         }
         if(isoutthere.Count > 0)
@@ -115,32 +118,25 @@ public class GameMaster : MonoBehaviour
         {
             for(int i=0;i<Input.touchCount;i++)
             {
-                // Touch touch = Input.GetTouch(0);
                 Touch[] touches = Input.touches;
-                Vector2 startPos = touches[i].rawPosition;
-                // Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+                Vector2 startPos = touches[i].rawPosition; //the first time the touch connected
                 if(touches[i].phase == TouchPhase.Ended && (touches[i].deltaPosition.x <-0.15 || touches[i].deltaPosition.x > 0.15 || touches[i].deltaPosition.y > 0.15 || touches[i].deltaPosition.x < -0.15))
                 {
                     double timetouched = time;
-                    Vector2 endPos = Input.touches[i].position;
-                    // Debug.Log(touches[i].deltaPosition);
+                    Vector2 endPos = Input.touches[i].position; //noting the endposition of the touch
                     double rad = Mathf.Atan2(touches[i].deltaPosition.y,touches[i].deltaPosition.x); 
-                    double angle = rad * (180 / Math.PI);
+                    double angle = rad * (180 / Math.PI); //check the angle and change it to degrees from radian
                     NoteDiamond s = touchable.Peek().getGameObject().GetComponent<NoteDiamond>();
-                    // Debug.Log(angle);
-                    if(getDir(angle,touchable.Peek().time)==true && (touchable.Peek().time + 1 - time < 0.38  && touchable.Peek().time + 1 - time > -0.15))
+                    if(getDir(angle,touchable.Peek().time)==true && (touchable.Peek().time + 1 - time < 0.38  && touchable.Peek().time + 1 - time > -0.15)) //just for the timing
                     {
-                        Debug.Log(touchable.Peek().time+1.5 - time);
                         s.setState("perfect");
                     }
                     else if(getDir(angle,touchable.Peek().time)==true && (touchable.Peek().time + 1 - time < 0.8  && touchable.Peek().time + 1 - time > -0.3))
                     {
-                        Debug.Log(touchable.Peek().time+1.5 - time);
                         s.setState("good");
                     }
                     else
                     {
-                        Debug.Log(touchable.Peek().time+1.5 - time);
                         s.setState("miss");
                     }
                     touchable.Dequeue();
@@ -148,7 +144,7 @@ public class GameMaster : MonoBehaviour
             }
         }
     }
-    private float assignrotate(char dir)
+    private float assignrotate(char dir) //this function is to check the note direction and assign it by looking at the deltaPosition
     {
         float z=0;
         if(dir =='w')
@@ -167,10 +163,11 @@ public class GameMaster : MonoBehaviour
             z = 270;
         else if(dir =='e')
             z = 315;
-        Debug.Log(z);
+        // Debug.Log(z);
         return z;
     }
-    /*
+
+        /*
         Direction :
         w = up;
         q = upleft;
@@ -180,7 +177,7 @@ public class GameMaster : MonoBehaviour
         c = downright;
         d = right;
         e = upright;
-    */
+        */
 
     private string checkDir(double angle)
     {
@@ -206,23 +203,15 @@ public class GameMaster : MonoBehaviour
 
     private bool getDir(double angle,double time)
     {
-        Debug.Log("timing = "+time);
-        Debug.Log(sametime.Count);
         int now = sametime.FindIndex(x => x.timing >= time);
-        foreach((double t,List<Note> n) s in sametime){
-            Debug.Log("AVAIL TIME = "+s.t);
-        }
         string dir = checkDir(angle);
-        Debug.Log("now = "+now+", dir = "+dir);
         if(now != -1){
             string dirs = "";
             foreach(Note note in sametime[now].notes){
                 dirs += note.direction + ", ";
             }
-            Debug.Log(time+" : (input)"+dir+", (selection)"+dirs);
             if(sametime[now].notes.FindIndex(x => x.direction[0] == dir[0]) != -1) {
                 Note sameDirNote = sametime[now].notes.Find(x => x.direction[0] == dir[0]);
-                Debug.Log("Removing "+sameDirNote.direction);
                 sametime[now].notes.Remove(sameDirNote);
                 if(sametime[now].notes.Count == 0){
                     sametime.Remove(sametime[now]);
